d331bcf82f293ba1c52f220e305690f41321e159
tiagorol <tiago.rol@gmail.com>
2013-10-04 10:06:36 -0300

    s

--- TesteGitBV/src/Novo.java
+++ TesteGitBV/src/Novo.java
@@ -3,5 +3,7 @@ public class Novo {
 	public static void main(String[] args) {
 		System.out.println("Alo");
 		System.out.println("Agora ficou show!!!");
+		System.out.println("Agora ficou show!!!");
+		System.out.println("Agora ficou show!!!");
 	}
 }
